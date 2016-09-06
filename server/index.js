92e3ca83c0f60689183f5142d6531c875d63ad9b
Keeley Nakamoto <keeleynakamoto@gmail.com>
2016-09-06 01:12:58 -0700

    Create html front-end skeleton and continue server and database buildout

--- server/index.js
+++ server/index.js
@@ -3,10 +3,30 @@ var express = require('express');
 
 var db = mysql.createConnection({
 	host: 'localhost',
-	//user: '',
-	//password: '',
-	database: 'Students'
+	user: 'root',
+	password: '',
+	database: 'students'
 });
 
-connection.connect();
+var app = express();
 
+db.connect(function(err) {
+	if (!err) {
+		console.log('Database is connected');
+	} else {
+		console.log('Error connecting database');
+	}
+});
+
+app.get('/', function(req, res) {
+	db.query('SELECT * from students', function(err, students) {
+		db.end();
+		if(!err) {
+			console.log('Matching students: ', students);
+		} else {
+			console.log('Error while performing query.');
+		}
+	})
+})
+
+app.listen(3000);
