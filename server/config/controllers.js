db0f249450cfd2f140f1b2bc71ebbb11127c9323
Keeley Nakamoto <keeleynakamoto@gmail.com>
2016-09-10 17:46:50 -0700

    Correct server and database helper functions

---
+++ server/config/controllers.js
@@ -0,0 +1,67 @@
+module.exports = {
+	getAll: function(callback) {
+		db.query('SELECT * FROM students', function(err, students) {
+			if(!err) {
+				callback(students);
+			} else {
+				console.log('Error while fetching all students.', err);
+			}
+		})
+
+	},
+
+	getOne: function(id, callback) {
+		db.query('SELECT * FROM students WHERE id  = ?', id, function(err, student) {
+			if (!err) {
+				callback(student);
+			} else {
+				console.log('Error while fetching student with id: ', id, ' ', err);
+			}
+		})
+	},
+
+	addOne: function(student, callback) {
+		var name = student.name;
+		var cohort = student.cohort;
+		db.query('INSERT INTO students SET name = ? cohort = ?', [name, cohort], function (err, newStudent) {
+			if (!err) {
+				callback(newStudent);
+			} else {
+				console.log('Error while adding student: ', name, ' ', err);
+			}
+		})
+	},
+
+	updateOne: function(id, student, callback) {
+		var name = student.name;
+		var cohort = student.cohort;
+		db.query('UPDATE students SET name = ? cohort = ? WHERE id = ?', [name, cohort, id], function(err, updatedStudent) {
+			if (!err) {
+				callback(newStudent);
+			} else {
+				console.log('There was an error while updating student: ', name, ' ', err);
+			}
+		})
+	},
+
+	deleteOne: function(id, callback) {
+		db.query('DELETE FROM students WHERE id = ?', id, function(err, deleted) {
+			if (!err) {
+				callback(deleted);
+			} else {
+				console.log('Error while deleting student with id: ', id, ' ', err)
+			}
+		})
+	},
+
+	deleteAll: function(cohort, callback) {
+		db.query('DELETE FROM students WHERE cohort = ?', cohort, function(err, deleted) {
+			if (!err) {
+				callback(deleted);
+			} else {
+				console.log('Error while deleting cohort: ', cohort, ' ', err);
+			}
+		})
+	}
+}
+
--- server/config/handlers.js
+++ server/config/handlers.js
@@ -1,73 +1,55 @@
 var mysql = require('mysql');
 var db = require('./db');
+var Students = require('./controllers');
+
 
 module.exports = {
 	getAllStudents: function(req,res) {
-		db.query('SELECT * FROM students', function(err, students) {
-			//db.end();
-			if(!err) {
-				console.log('Matching students: ', students);
-			} else {
-				console.log('Error while fetching all students.', err);
-			}
+		Students.getAll(function(err, students) {
+			if (err) {return res.sendStatus(400);}
+			res.send(students);
 		})
 
 	},
 
 	getOneStudent: function(req, res) {
-		var id = req.params._id;
-		db.query('SELECT * FROM students WHERE id  = ?', id, function(err, student) {
-			if (!err) {
-				console.log ('Matching student: ', student);
-			} else {
-				console.log('Error while fetching student with id: ', id, ' ', err);
-			}
+		var id = req.params.id;
+		Students.getOne(id, function(err, student) {
+			if (err) { return res.setStatus(400);}
+			res.send(student);
 		})
 	},
 
 	addOneStudent: function(req, res) {
-		var name = req.params.name;
-		var cohort = req.params.cohort;
-		db.query('INSERT INTO students SET name = ? cohort = ?', [name, cohort], function (err, student) {
-			if (!err) {
-				console.log('Student has been added: ', student);
-			} else {
-				console.log('Error while adding student: ', name, ' ', err);
-			}
+		var student = req.body;
+		Students.addOne(name, cohort, function(err, addedStudent) {
+			if (err) { return res.sendStatus(400);}
+			res.send(addedStudent);
 		})
 	},
 
 	updateOneStudent: function(req, res) {
-		var id = req.params._id;
-		var name = req.params.name;
-		db.query('UPDATE students SET name = ? WHERE id = ?', [name, id], function(err, student) {
-			if (!err) {
-				console.log('The student has been updated: ', student);
-			} else {
-				console.log('There was an error while updating student: ', name, ' ', err);
-			}
+		var id = req.params.id;
+		var student = req.body;
+		Students.updateOne(id, student, function(err, updatedStudent) {
+			if (err) { return res.sendStatus(400);}
+			res.send(updatedStudent);
 		})
 	},
 
 	deleteOneStudent: function(req, res) {
-		var id = req.params._id;
-		db.query('DELETE FROM students WHERE id = ?', id, function(err, deleted) {
-			if (!err) {
-				console.log('Deletion successful', deleted);
-			} else {
-				console.log('Error while deleting student with id: ', id, ' ', err)
-			}
+		var id = req.params.id;
+		Students.deleteOne(id, function(err, student) {
+			if (err) { return res.sendStatus(400);}
+			res.status(200);
 		})
 	},
 
 	deleteCohort: function(req, res) {
-		var cohort = req.params.cohort;
-		db.query('DELETE FROM students WHERE cohort = ?', cohort, function(err, deleted) {
-			if (!err) {
-				console.log('Deleted all students in cohort: ', cohort);
-			} else {
-				console.log('Error while deleting cohort: ', cohort, ' ', err);
-			}
+		var cohort = req.headers.cohort;
+		Students.deleteAll(cohort, function(err, cohort) {
+			if (err) { return res.sendStatus(400);}
+			res.status(200);
 		})
 	}
 }
